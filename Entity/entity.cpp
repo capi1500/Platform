b1cd3c19ad403d8e4d71a91a0afbce8c49986796
capi1500 <kacper.chetkowski@gmail.com>
2018-07-07 23:27:52 +0200

    Little bugfix

--- Entity/entity.cpp
+++ Entity/entity.cpp
@@ -19,7 +19,7 @@ bool Entity::onGround(){
 	for(auto* contact = getBody()->GetContactList(); contact; contact = contact->next){
 		for(auto obj : object){
 			if(contact->other == obj->getBody() and contact->contact->IsTouching()){
-				if(obj->getObjectType() == ObjectType::Ground){
+				if(obj->getObjectType() == ObjectType::Ground or obj->getObjectType() == ObjectType::PhysicalObject){
 					sf::FloatRect o1 = getGlobalBounds(), o2 = obj->getGlobalBounds();
 					if(o1.top + o1.height <= o2.top and o1.left + o1.width > o2.left and o1.left < o2.left + o2.width){
 						return true;
